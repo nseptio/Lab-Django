2c64ab378d09127a74bf19489076238a9f4ad31e
Septio Nugroho <septio.nugroho@ui.ac.id>
2022-09-22 15:55:24 +0700

    Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,7 +4,9 @@ from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_xml_by_id #sesuaikan dengan nama fungsi yang dibuat
-
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'wishlist'
 
@@ -14,4 +16,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ]
